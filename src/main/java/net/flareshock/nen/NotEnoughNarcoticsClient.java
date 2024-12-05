66045386a49fcfd34e731045a17b477c075662d2
AlanListDev <alansedmkstreams@email.cz>
2024-12-05 13:36:20 +0100

    Initial commit

---
+++ src/main/java/net/flareshock/nen/NotEnoughNarcoticsClient.java
@@ -0,0 +1,10 @@
+package net.flareshock.nen;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NotEnoughNarcoticsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/flareshock/nen/NotEnoughNarcoticsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.flareshock.nen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NotEnoughNarcoticsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
